827ee2b9b3b635446bb415a1ff5c325994d05613
igl-myrick <igl.myrick@gmail.com>
2024-08-16 13:58:13 -0700

    add first test for Pastry object

---
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+using System;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry();
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+  }
+}
