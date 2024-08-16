dfb3bef90033400dfc00ae31735c75398b2feea4
igl-myrick <igl.myrick@gmail.com>
2024-08-16 15:08:06 -0700

    add CalculatePrice method

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -5,5 +5,22 @@ namespace Bakery.Models
   public class Pastry
   {
     public int Price { get; } = 2;
+
+    public int CalculatePrice(int amtToBuy)
+    {
+      int outputPrice = 2;
+      for (int index = 0; index < amtToBuy; index++)
+      {
+        if (index % 4 != 0)
+        {
+          outputPrice += 2;
+        }
+        else
+        {
+          outputPrice += 0;
+        }
+      }
+      return outputPrice;
+    }
   }
 }
